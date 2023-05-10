451ba58bb8294cb71a7c893953272a27c7af23dc
Nitin Shirale <nitinshirale051@gmail.com>
2023-05-10 17:20:16 +0530

    test
    test

--- src/com/nitinshirale/spring/entity/Customer.java
+++ src/com/nitinshirale/spring/entity/Customer.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//entity
 @Entity
 @Table(name="customer")
 public class Customer {
